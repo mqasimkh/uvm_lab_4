414550a07e47b1d173c49ec2735be75056cc75cf
Muhammad Qasim <154578365+mqasimkh@users.noreply.github.com>
2025-06-29 14:28:52 +0500

    ...

--- code/yapp/sv/yapp_pkg.sv
+++ code/yapp/sv/yapp_pkg.sv
@@ -4,12 +4,12 @@ package yapp_pkg;
 
     typedef uvm_config_db#(virtual yapp_if) yapp_vif_config;
 
-    `include "./sv/yapp_packet.sv"
-    `include "./sv/yapp_tx_monitor.sv"
-    `include "./sv/yapp_tx_sequencer.sv"
-    `include "./sv/yapp_tx_seqs.sv"
-    `include "./sv/yapp_tx_driver.sv"
-    `include "./sv/yapp_tx_agent.sv"
-    `include "./sv/yapp_tx_env.sv"
+    `include ".yapp_packet.sv"
+    `include ".yapp_tx_monitor.sv"
+    `include ".yapp_tx_sequencer.sv"
+    `include "yapp_tx_seqs.sv"
+    `include "yapp_tx_driver.sv"
+    `include "yapp_tx_agent.sv"
+    `include "yapp_tx_env.sv"
 
 endpackage: yapp_pkg
